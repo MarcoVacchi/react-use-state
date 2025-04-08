1d92358a93ef980d940a2adf6d1d19849a7648eb
MarcoVacchi <mvacchi816@gmail.com>
2025-04-08 15:42:59 +0200

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,31 @@
+
+function Main() {
+
+    return (
+        <>
+            <h1 className="text-center mt-5">Learn web devlopment</h1>
+            <div className="container d-flex justify-content-evenly bg-secondary-subtle mt-5">
+                <button type="button" className="btn btn-primary btn-sm">HTML</button>
+                <button type="button" className="btn btn-primary btn-sm">CSS</button>
+                <button type="button" className="btn btn-primary btn-sm">javaScript</button>
+                <button type="button" className="btn btn-primary btn-sm">Node.js</button>
+                <button type="button" className="btn btn-primary btn-sm">Express</button>
+                <button type="button" className="btn btn-primary btn-sm">ReactJS</button>
+            </div>
+
+        </>
+    )
+}
+
+export default Main;
+
+{/* <div className="container d-flex justify-content-evenly bg-secondary-subtle mt-5">
+
+                <button type="button" className="btn btn-primary btn-sm">HTML</button>
+                <button type="button" className="btn btn-primary btn-sm">CSS</button>
+                <button type="button" className="btn btn-primary btn-sm">javaScript</button>
+                <button type="button" className="btn btn-primary btn-sm">Node.js</button>
+                <button type="button" className="btn btn-primary btn-sm">Express</button>
+                <button type="button" className="btn btn-primary btn-sm">ReactJS</button>
+
+            </div> */}
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import languages from "../../data/languages";
+console.log(languages)
+
+
+function MyComponent() {
+
+  const
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
+
+export default MyComponent;
